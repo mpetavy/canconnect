0a79994c5a8d7d593652610a14405570911019a4
mpetavy <mpetavy>
2019-07-23 13:54:10 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+## CANCONNECT
+### Project description of CANCONNECT
+This is the automatic generated project description of CANCONNECT. Stay tuned for updates :-)
+### License
+All software is copyright and protected by the Apache License, Version 2.0.
+https://www.apache.org/licenses/LICENSE-2.0
---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"net"
+	"fmt"
+	"flag"
+	"time"
+	"github.com/mpetavy/common"
+)
+
+var (
+	address *string
+	udp     *bool
+)
+
+func init() {
+	address = flag.String("c", "", "server:port to test")
+}
+
+func run() error {
+	conn, err := net.Dial("tcp", *address)
+	if err != nil {
+		return err
+	}
+
+	defer conn.Close()
+
+	fmt.Printf("%s connected successfully ", *address)
+
+	return nil
+}
+
+func main() {
+	defer common.Cleanup()
+
+	common.New(&common.App{"canconnect", "1.0.0", "2019", "Can connect to server:port", "mpetavy", common.APACHE, "https://github.com/mpetavy/canconnect", false, nil, nil, nil, run, time.Duration(0)}, []string{"c"})
+	common.Run()
+}
